ba9b71668b12521618cdc926b86e497651ccc22d
koosb923 <115927883+koosb923@users.noreply.github.com>
2022-12-20 17:02:09 +0900

    Initial Commit

---
+++ Phomean/PhomeanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhomeanApp.swift
+//  Phomean
+//
+//  Created by 구성범 on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct PhomeanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
